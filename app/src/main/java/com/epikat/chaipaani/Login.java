13a8da068a889dcac3a79db9094f7cab881630df
Tushar Narang <tusharnarang1994@gmail.com>
2019-03-26 22:17:48 +0530

    menu activity + api call try

--- app/src/main/java/com/epikat/chaipaani/Login.java
+++ app/src/main/java/com/epikat/chaipaani/Login.java
@@ -1,27 +1,118 @@
 package com.epikat.chaipaani;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
 
 public class Login extends AppCompatActivity {
 
     Button login;
+    EditText emailid, password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        emailid = (EditText) findViewById(R.id.Login_emailid);
+        password = (EditText) findViewById(R.id.Login_password);
         login = (Button) findViewById(R.id.Login_loginButton);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(Login.this, Dashboard.class);
-                startActivity(i);
-                finish();
+                new checkLogin().execute(emailid.getText().toString(), password.getText().toString());
             }
         });
+    }
+
+
+    public class checkLogin extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+                String URL = "http://chaipani.herokuapp.com/api/login";
+                JSONObject jsonBody = new JSONObject();
+                jsonBody.put("email", strings[0]);
+                jsonBody.put("password", strings[1]);
+                final String requestBody = jsonBody.toString();
+
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.w("response", response);
+                        // Log.i("VOLLEY", response);
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.w("error", error.toString());
+                    }
+                }) {
+                    @Override
+                    public String getBodyContentType() {
+                        return "application/json; charset=utf-8";
+                    }
+
+                    @Override
+                    public byte[] getBody() throws AuthFailureError {
+                        try {
+                            return requestBody == null ? null : requestBody.getBytes("utf-8");
+                        } catch (UnsupportedEncodingException uee) {
+                            VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", requestBody, "utf-8");
+                            return null;
+                        }
+                    }
+
+                    @Override
+                    protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                        String token = "";
+                        if (response != null) {
+                            try{
+                                String responseString = new String(response.data);
+                                JSONObject obj = new JSONObject(responseString);
+                                token = obj.getJSONObject("data").getString("token");
+                                Log.w("token", token);
+
+                                Intent i = new Intent(Login.this, Dashboard.class);
+                                startActivity(i);
+                                finish();
+
+                            }catch (JSONException e){
+
+                            }
+
+                        }
+                        return Response.success(token, HttpHeaderParser.parseCacheHeaders(response));
+                    }
+                };
+
+                requestQueue.add(stringRequest);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 }
--- app/src/main/java/com/epikat/chaipaani/MainActivity.java
+++ app/src/main/java/com/epikat/chaipaani/MainActivity.java
@@ -1,21 +1,44 @@
 package com.epikat.chaipaani;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Handler;
+import android.preference.PreferenceActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+import cz.msebera.android.httpclient.Header;
+
 public class MainActivity extends AppCompatActivity {
 
-    public Connection DbConn;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,36 +49,32 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 //Do something after 100ms
-                ConnectToDatabase();
-//                Intent i = new Intent(MainActivity.this, Login.class);
-//                startActivity(i);
-//                finish();
+
+                Intent i = new Intent(MainActivity.this, Login.class);
+                startActivity(i);
+                finish();
             }
         }, 2000);
 
+//        RequestParams params = new RequestParams();
+//        params.put("email", "Bhavesh.Gulecha@iiitb.org");
+//        params.put("password", "12345678");
+//        client.post("chaipani.herokuapp.com/api/login", params, new AsyncHttpResponseHandler() {
+//            @Override
+//            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+//                Log.w("success", responseBody.toString());
+//            }
+//
+//            @Override
+//            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+//                Log.w("failure", responseBody.toString());
+//            }
+//        });
+
     }
 
-    public void ConnectToDatabase(){
-        try {
 
-            // SET CONNECTIONSTRING
-            Class.forName("net.sourceforge.jtds.jdbc.Driver").newInstance();
-            String username = "chaipaani";
-            String password = "chaipaani";
-            DbConn = DriverManager.getConnection("chaipaani.chs6waedsdkl.us-east-1.rds.amazonaws.com;user=" + username + ";password=" + password);
 
-            Statement stmt = DbConn.createStatement();
-            ResultSet reset = stmt.executeQuery(" select * from users ");
-
-            Log.w("Got result set", "here:" + reset.toString());
-            TextView num = (TextView) findViewById(R.id.main_appname);
-            num.setText(reset.getString(1));
-
-            DbConn.close();
-
-        } catch (Exception e)
-        {
-            Log.w("Error connection","" + e.getMessage());
-        }
-    }
 }
+
+
